5f98c4b4680f551e6b713df04e80449be92e037b
FellipeF <fellipefreire99@hotmail.com>
2024-08-08 21:11:19 -0300

    Created interfaces Seller and Department DAO

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+//Control Seller operations on DB
+
+import java.util.List;
+import model.entities.Seller;
+
+public interface SellerDao {
+    void insert(Seller d);
+    void update(Seller d);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
