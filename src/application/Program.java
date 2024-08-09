d1915b936c7ce0c6241d61a18b4d21205d203771
FellipeF <fellipefreire99@hotmail.com>
2024-08-08 21:05:40 -0300

    Created Seller class as entity

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,20 @@
 package application;
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
     public static void main(String[] args) {
         //Test Department Class
-        
         Department d = new Department(1, "Books");
+        
+        //Test Seller Class
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, d);
+        
         System.out.println(d);
+        System.out.println(seller);
     }
     
 }
