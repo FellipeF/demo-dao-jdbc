8a92228df9e74437111fe76cec243872ab9a91b2
FellipeF <fellipefreire99@hotmail.com>
2024-08-08 22:18:22 -0300

    SellerDAOJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.util.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,9 @@ public class Program {
         
         //Test Seller Class
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, d);
+        
+        //Test Seller DAO through dependency injection.
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         
         System.out.println(d);
         System.out.println(seller);
